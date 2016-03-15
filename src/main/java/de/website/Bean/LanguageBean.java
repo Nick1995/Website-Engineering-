6c03c8a86887fbc3647271bced7ad246bbf9e92f
Tobi <tobb-k@gmx.de>
2016-03-15 21:35:03 +0100

    Layout angepasst

--- src/main/java/de/website/Bean/LanguageBean.java
+++ src/main/java/de/website/Bean/LanguageBean.java
@@ -4,6 +4,7 @@ package de.website.Bean;
  * Created by Tobi on 16.02.2016.
  */
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
@@ -12,10 +13,11 @@ import java.util.Locale;
 import java.util.Map;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ValueChangeEvent;
+import javax.inject.Scope;
 
 
 @ManagedBean(name="language")
-@SessionScoped
+@ApplicationScoped
 public class LanguageBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
